667fee4b1d7a56268fcf12592e4568960474a8d4
Francisco Antonio Murillo Pacheco <43586013+fmurillopacheco@users.noreply.github.com>
2020-04-26 14:55:38 +0200

    Se modifica index.html introduciendo el menú de JSP. Se crea servlet FormComplejo. Se crea formComplejo.html en carpeta html.
    25-04-2020
    by Francisco Antonio Murillo Pacheco
    
    -----------------------------------------------------------------------------------------------------------
    
    Se realizan cambios en el formComplejo.html
    A la hora de rediriguir al servlet no me coge bien la direccion.
    
    
    26-04-2020
    by Francisco Antonio Murillo Pacheco

---
+++ src/main/java/es/albarregas/controllers/FormComplejo.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Francisco_Antonio
+ */
+@WebServlet(name = "FormComplejo", urlPatterns = {"/FormComplejo"})
+public class FormComplejo extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FormComplejo</title>");
+            out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"CSS/estilo.css\">");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet FormComplejo at " + request.getContextPath() + "</h1>");
+            
+            Map<String, String[]> parametros = request.getParameterMap();
+            for(Map.Entry<String, String[]> entrada : parametros.entrySet()){
+                if(!entrada.getKey().startsWith("env")){
+                    out.println("<p>" + entrada.getKey()+":");
+                    
+                    for(String valor : entrada.getValue()){
+                        out.println("<strong>");
+                        out.println(valor);
+                        out.println("</strong>");  
+                    }
+                    out.println("</p>");
+                }
+            }
+            
+            out.println("<p align=\"center\"><a href=\"index.html\">Menú Principal</a></p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
